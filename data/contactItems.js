b013c82e113257f132aac80c73822219b29d38f8
Martina Papp <martinapapp0@gmail.com>
2026-03-13 13:33:31 +0100

    fix: resume path

--- data/contactItems.js
+++ data/contactItems.js
@@ -25,7 +25,7 @@ export const contactItems = [
   },
   {
     label: 'Resume',
-    href: '../resume.pdf',
+    href: '/resume.pdf',
     address: 'Resume',
     svg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
       <path d="M14 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V8l-6-6zm-1 7V3.5L18.5 9H13zM8 13h8v1.5H8V13zm0 3h8v1.5H8V16zm0-6h3v1.5H8V10z"/>
